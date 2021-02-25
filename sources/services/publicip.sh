097ea75a81bff53c81f198010faf9fcaa1d3d94c
JacoV <jvosloo@imqs.co.za>
2021-02-26 00:02:04 +0200

    Fix service scripts and formatting

--- sources/services/publicip.sh
+++ sources/services/publicip.sh
@@ -1,1 +1,6 @@
-curl http://ip4only.me/api/
+#!/bin/bash
+while :; do
+  curl "https://www.duckdns.org/update?domains=bosvos&token=a971f1bd-1d21-49bd-a94a-0da5e7a18d4f&ip="
+  echo "sleeping"
+  sleep 300s
+done
